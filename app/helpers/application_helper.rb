37abd0070630680588d809b0540d08347ba811cc
leva84 <denfrut8@gmail.com>
2023-05-27 11:48:24 +0300

    4 Added heder to site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sign_up_button
+    button_to t('buttons.sign_up'), new_user_registration_path, class: 'text-white btn btn-secondary btn-sm'
+  end
+
+  def sign_in_button
+    button_to t('buttons.sign_in'), new_user_session_path, class: 'text-white btn btn-secondary btn-sm'
+  end
+
+  def sign_out_button
+    button_to t('buttons.sign_out'), destroy_user_session_path, method: :delete, class: 'text-white btn btn-secondary btn-sm'
+  end
 end
