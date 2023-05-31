e7ac80d77e616eb4247585e0c7af0daa27a68bfe
leva84 <denfrut8@gmail.com>
2023-06-01 00:40:06 +0300

    6 Fixed cops

--- spec/features/users/sessions/new_spec.rb
+++ spec/features/users/sessions/new_spec.rb
@@ -1,9 +1,9 @@
-RSpec.feature 'User create their session', type: :feature do
-  scenario 'visiting the new page', js: true do
+RSpec.describe 'User create their session', type: :feature do
+  it 'visiting the new page', js: true do
     visit new_user_registration_path
-    expect(page).to have_field("user[password]")
-    expect(page).to have_field("user[password]")
-    expect(page).to have_field("user[password_confirmation]")
+    expect(page).to have_field('user[password]')
+    expect(page).to have_field('user[password]')
+    expect(page).to have_field('user[password_confirmation]')
     expect(page).to have_button(t('buttons.sign_up'))
   end
 end
